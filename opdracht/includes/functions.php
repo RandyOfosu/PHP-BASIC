894b95ba7e6ef9606c3960f0c004fd037cd77b57
Randy Ofosu <randy_ofosu@hotmail.com>
2020-06-01 13:12:16 +0200

    eindopdracht

--- opdracht/includes/functions.php
+++ opdracht/includes/functions.php
@@ -24,4 +24,34 @@ function getAge()
     echo $leeftijd;
 }
 
+function getTotaal(){
+    $prijs = array(0.20,0.12,0.30,0.40,0.20,0.15,1.20,0.80,0.60,0.20);
+    $productnummer = $_GET['productnummer'];
+    $aantalProd = $_GET['aantal'];
+    echo $prijs[$productnummer] * $aantalProd;
+}
+
+function getKorting(){
+    $leeftijd = $_GET['leeftijd'];
+    switch ($leeftijd) {
+        case 19:
+          echo "0,19";
+          break;
+        case 18:
+          echo "0,18";
+          break;
+        case 17:
+          echo "0,17";
+          break;
+        case 16:
+          echo "0,16";
+          break;
+        case $leeftijd < 16:
+          echo "1,00";
+          break;
+        default:
+          echo "Uw Korting";
+      }
+}
+
 ?>
--- opdracht/producten.php
+++ opdracht/producten.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <?php include 'includes/menu.php';?>
+    <?php include 'includes/functions.php';?>
     <title>Title</title>
   </head>
     <form action="producten.php" method="get"><!-- dit laten staan! -->
@@ -14,35 +15,17 @@
     
     $producten = array("appels", "peren", "aardbei", "mango", "meloen", "aardappel", "banaan", "wortel", "prei", "ui", "bosui");
     $prijs = array(0.25, 0.30, 0.10, 0.40, 1.20, 0.15, 0.30, 0.20, 0.10, 0.50, 0.30);
-    
-    getKorting();
 
-    function getPrijs(){
-        
-    }
-
-    function getKorting(){
-    $leeftijd = $_GET['leeftijd'];
-    switch ($leeftijd) {
-        case 19:
-          echo "0,19";
-          break;
-        case 18:
-          echo "0,18";
-          break;
-        case 17:
-          echo "0,17";
-          break;
-        case 16:
-          echo "0,16";
-          break;
-        case $leeftijd < 16:
-          echo "1,00";
-          break;
-        default:
-          echo "Uw Korting";
-      }
-}
+    echo "<tr>" . "<td>" . "0. " . $producten[0] . "<td>" . $prijs[0] . "<br>";
+    echo "<tr>" . "<td>" . "1. " . $producten[1] . "<td>" . $prijs[1] . "<br>";
+    echo "<tr>" . "<td>" . "2 " . $producten[2] . "<td>" . $prijs[2] . "<br>";
+    echo "<tr>" . "<td>" . "3. " . $producten[3] . "<td>" . $prijs[3] . "<br>";
+    echo "<tr>" . "<td>" . "4. " . $producten[4] . "<td>" . $prijs[4] . "<br>";
+    echo "<tr>" . "<td>" . "5. " . $producten[5] . "<td>" . $prijs[5] . "<br>";
+    echo "<tr>" . "<td>" . "6. " . $producten[6] . "<td>" . $prijs[6] . "<br>";
+    echo "<tr>" . "<td>" . "7. " . $producten[7] . "<td>" . $prijs[7] . "<br>";
+    echo "<tr>" . "<td>" . "8. " . $producten[8] . "<td>" . $prijs[8] . "<br>";
+    echo "<tr>" . "<td>" . "9. " . $producten[9] . "<td>" . $prijs[9];
 
     ?>
     <p>Vul hieronder het productnummer in van het product wat je wilt bestellen en de hoeveelheid.</p> 
@@ -51,8 +34,8 @@
     <p>Vul je leeftijd in voor korting</p>
     <p><label>leeftijd: </label><input type="text" name="leeftijd"></p><!-- dit laten staan! -->
     <p><button type="submit" name="submit">Bereken totaal</button></p><!-- dit laten staan! -->
-    <p>voor <?php getAmount(); ?> x het product: <?php getProductNr(); ?> betaal je:</p>
-    <p>je bent <?php getAge(); ?> je korting is: </p>
+    <p>voor <?php getAmount(); ?> x het product: <?php getProductNr(); ?> betaal je:</p> <?php getTotaal() ?>
+    <p>je bent <?php getAge(); ?> je korting is: </p> <?php getKorting() ?>
 
     </form><!-- dit laten staan! -->
     <footer><?php include 'includes/footer.php'; ?></footer>
